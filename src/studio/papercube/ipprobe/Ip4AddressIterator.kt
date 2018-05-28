87cf9892492ba43949b8c3b8c7031d98755805a6
PaperCube <imzhy@hotmail.com>
2018-05-28 20:37:39 +0800

    Fix: Ip4AddressIterator skips first element

--- src/studio/papercube/ipprobe/Ip4AddressIterator.kt
+++ src/studio/papercube/ipprobe/Ip4AddressIterator.kt
@@ -2,15 +2,20 @@ package studio.papercube.ipprobe
 
 class Ip4AddressIterator(private val from: Ip4Address, private val to: Ip4Address) : Iterator<Ip4Address> {
     private var current = from
+    private var firstElementConsumed = false
 
     override fun hasNext(): Boolean {
-        return current != to
+        return current < to
     }
 
     override fun next(): Ip4Address {
-        if (hasNext()) {
-            current += 1
-            return current
-        } else throw NoSuchElementException()
+        try {
+            if (hasNext()) {
+                if (firstElementConsumed) current += 1
+                return current
+            } else throw NoSuchElementException()
+        } finally {
+            firstElementConsumed = true
+        }
     }
 }
